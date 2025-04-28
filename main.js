ea3ad0f7dc42d24acc7dcafab88b2f6c112df151
Erika Zanin <erikaczbu@gmail.com>
2025-04-28 20:48:32 -0300

    Atualizando layout mobile

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ document.querySelectorAll('.btnList').forEach(item => {
         // Ajusta a largura de acordo com o tamanho da tela
         if (isSmallScreen) {
           item.style.width = '60%';
-          content.style.marginTop =  '180px';
+          content.style.marginTop =  '310px';
           body.style.height = '110%';
           html.style.height = '110%';
         } else {
